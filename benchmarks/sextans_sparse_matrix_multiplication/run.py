3ecb85d28d33daed20d33881a9e28de04178014a
Jason Lau <lau@rapidstream-da.com>
2024-04-02 14:21:15 -0700

    refactor: simplify sextans script

--- benchmarks/sextans_sparse_matrix_multiplication/run.py
+++ benchmarks/sextans_sparse_matrix_multiplication/run.py
@@ -3,17 +3,14 @@ Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.  All ri
 The contributor(s) of this file has/have agreed to the RapidStream Contributor License Agreement.
 """
 
-from pathlib import Path
+from rapidstream import get_u280_vitis_device_factory, RapidStreamTAPA
+
+INI_PATH = "./design/config/link_config.ini"
+VITIS_PLATFORM = "xilinx_u280_gen3x16_xdma_1_202211_1"
+XO_PATH = "./design/generated/Sextans.xo"
 
 
-from rapidstream.assets.device_library.u280.u280 import get_u280_vitis_device_factory
-from rapidstream.assets.ui.tapa import RapidStreamTAPA
-
-INI_PATH = Path("./design/config/link_config.ini")
-XO_PATH = Path("./design/generated/Sextans.xo")
-
-
-factory = get_u280_vitis_device_factory("xilinx_u280_gen3x16_xdma_1_202211_1")
+factory = get_u280_vitis_device_factory(VITIS_PLATFORM)
 
 # Reserve resources for the HBM Memory Sub-System.
 # The HMSS is not part of the user kernel so the partition optimization process
@@ -32,14 +29,14 @@ factory.reduce_slot_area(0, 0, lut=5000 * 13, ff=6500 * 13)
 # For this U280 platform, the right most DSP column on the boundary between
 # dynamic/static region is not usable. So we need to adjust the DSP count
 # to reflect the actual available DSPs.
-print("HACK: Reducing DSP of (1, 1) to make it less congested")
+print("Reducing DSP of (1, 1) to make it less congested")
 factory.reduce_slot_area(1, 1, dsp=100)
 
 rs = RapidStreamTAPA("./build")
 
 rs.set_virtual_device(factory.generate_virtual_device())
 rs.add_xo_file(XO_PATH)
-rs.set_vitis_platform("xilinx_u280_gen3x16_xdma_1_202211_1")
+rs.set_vitis_platform(VITIS_PLATFORM)
 rs.set_vitis_connectivity_config(INI_PATH)
 
 rs.set_top_module_name("Sextans")
@@ -47,7 +44,7 @@ rs.add_clock("ap_clk", 3.33)
 
 rs.add_flatten_targets(["Sextans"])
 
-# ports bind to HBM 16-31
+# Bind ports to HBM 16-31
 right_slot = "SLOT_X1Y0:SLOT_X1Y0"
 left_slot = "SLOT_X0Y0:SLOT_X0Y0"
 
@@ -125,19 +122,19 @@ right_args = [
 for arg in right_args:
     rs.assign_port_to_region(f"m_axi_{arg}_.*", right_slot)
 
-# constrain the remaining control ports. All ports must be constrained to a specific
-# slot
+# Constrain the remaining control ports.
+# All ports must be constrained to a specific slot:
 rs.assign_port_to_region("s_axi_control_.*", left_slot)
 rs.assign_port_to_region("ap_clk", left_slot)
 rs.assign_port_to_region("ap_rst_n", left_slot)
 rs.assign_port_to_region("interrupt", left_slot)
 
-# you can customizer the placement strategy like this
+# Xustomize the placement strategy:
 rs.set_placement_strategy("EarlyBlockPlacement")
 
-# allow two parallel Vitis implementation
+# Allow two parallel Vitis implementation
 rs.run_dse(
-    max_workers=4,
+    max_workers=2,
     max_dse_limit=0.85,
     min_dse_limit=0.75,
     partition_strategy="flat",
