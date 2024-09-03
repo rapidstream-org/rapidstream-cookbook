7b0317d2b478bd4ecb51bcb052b53775941756cf
RipperJ <1628079212@qq.com>
2024-09-04 00:15:06 +0800

    fix(serpens): fix errors in run_tapa script

--- benchmarks/tapa_flow/serpens/design/run_tapa.sh
+++ benchmarks/tapa_flow/serpens/design/run_tapa.sh
@@ -5,12 +5,12 @@ WORK_DIR=generated
 
 tapac \
   --work-dir ${WORK_DIR} \
-  --top Sextans \
+  --top Serpens \
   --part-num xcu280-fsvh2892-2L-e \
   --clock-period 3.33 \
-  -o ${WORK_DIR}/Sextans.xo \
-  --connectivity config/link_config.ini \
-  src/sextans.cpp \
+  -o ${WORK_DIR}/Serpens.xo \
+  --connectivity config/link_config_a24.ini \
+  src/serpens-noconst.cpp \
   2>&1 | tee ${WORK_DIR}/tapa.log
 
-# change `src/sextans.cpp` to `src/sextans-no-const-para.cpp` to generate the design without aggressively passing constants through handshake interfaces.
+# change `src/sextans.cpp` to `src/serpens-noconst.cpp` to generate the design without aggressively passing constants through handshake interfaces.
