bade709943fdf59c3a24605c8d84fc051cd5b491
ylxiao <ylxiao@rapidstream-da.com>
2024-09-08 21:54:27 -0700

    feat(tapa) decrease knn_chipknn to 8 PEs

---
+++ benchmarks/tapa_flow/knn_chipknn/k2D_float_8PEs/design/run_tapa.sh
@@ -0,0 +1,9 @@
+WORK_DIR=work.out
+
+tapa compile \
+  --top Knn \
+  --part-num xcu280-fsvh2892-2L-e \
+  --clock-period 3.33 \
+  -o ${WORK_DIR}/knn.xo \
+  -f src/knn.cpp \
+  2>&1 | tee tapa.log
